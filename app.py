543a306a8b41f385589fa93e30497bb7b4c40fd2
unknown <annie227314021@gmail.com>
2023-05-12 03:24:52 +0800

    upload

--- app.py
+++ app.py
@@ -13,6 +13,13 @@ def base():
         
     )
 
+    folium.Marker(
+        location=[25.030797164609954, 121.55874457225863],
+        tooltip ="好玩嘗試xddd"
+
+    ).add_to(map)
+
+
 
     return map._repr_html_()
 
