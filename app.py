e14643044a7241c391e4aa6e6803191219f04e2c
unknown <annie227314021@gmail.com>
2023-05-12 03:00:06 +0800

    make it better

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 from flask import Flask
-app=Flask(__name__)
 
-@app.route("/")
-def index():
-    return "hello"
-if __name__ == "__main__":
-    app.run()
+import folium
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def base():
+    map = folium.Map(
+        location=[25.030797164609954, 121.55874457225863],
+        zoom_start=12,
+        popup='Skytree'
+    )
+
+    folium.Marker(
+        location=[25.030797164609954, 121.55874457225863],
+        
+        tooltip="好玩的嘗試XDDDD"
+
+    ).add_to(map)
+
+    return map._repr_html_()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
