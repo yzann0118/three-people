8a21e977c01722d9b347b1cf74683180979a93c2
yzann0118 <135314186+yzann0118@users.noreply.github.com>
2023-06-03 14:24:16 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, Response
+from flask import Flask, request, render_template, Response, json
 import folium
 from ultralytics import YOLO
 import cv2
@@ -21,7 +21,7 @@ def gen_result_frames(camera_id):
         if success:
             # Run YOLOv8 inference on the frame
             # results = model.track(frame)
-            results = model.predict(frame)
+            results = model.predict(frame, device='cpu')
             result = results[0].plot()
             ret, buffer = cv2.imencode('.jpg', result)
             result = buffer.tobytes()
@@ -38,109 +38,51 @@ def video_feed(camera_id):
 def detect(camera_id):
     if (int(camera_id)>400):
         return 'Wrong Camera ID !!'
-    return render_template('cameratest.html', camera_id=camera_id)
+    return render_template('camera_predict.html', camera_id=camera_id)
 
 @app.route('/CCTVmap')
 def CCTVmap():
     return render_template('臺北市CCTV設施地圖.html')
 
+@app.route('/accident/30days/<year>')
+def Days30(year):
+    path = '30日_'+year+'.html'
+    return render_template(path, year=year)
+
+@app.route('/accident/unsignalized/<year>')
+def Unsignal(year):
+    path = '無號誌_'+year+'.html'
+    return render_template(path, year=year)
+
+@app.route('/accident/pendestrian/<year>')
+def Pendestrian(year):
+    path = '行人涉入_'+year+'.html'
+    return render_template(path, year=year)
+
+@app.route('/accident/total/<year>')
+def Total(year):
+    path = '交通事故_'+year+'.html'
+    return render_template(path, year=year)
 
 @app.route('/service')
 def service_page():
-
     return render_template('111年臺北市無號誌交叉口原始點資料_縮放人數_new.html')
+
 @app.route('/about')
 def about_page():
-
     return render_template('about.html')
+
 @app.route('/contact')
 def contact_page():
-
     return render_template('contact.html')
+
 @app.route('/home')
-
 def home_page():
-
     return render_template('index.html')
 
 @app.route('/map')
-
 def map_page():
-
     return render_template('map.html')
-
-
-
-
-# @app.route('/', methods=['POST', 'GET'])
-
-
-# def base():
-#     map = folium.Map(
-#         location=[25.030797164609954, 121.55874457225863],
-#         zoom_start=12
-        
-#     )
-#     map.get_root().html.add_child(folium.Element("""
-#     <html>
-
-#     <head>
-#         <title>三人組道安平台</title>
-#     </head>
-#     <body>
-#         <div style ="position : fixed; left:1200px;top:30 px;width:400px;height:2000px;background-color:white;z-index:500">
-        
-#         <form>
-            
-#             <br>區域選擇  <select name="Location">
-#             <option value="Taipei">台北</option>
-#             <option value="Taoyuan">桃園</option>
-#             <option value="Hsinchu">新竹</option>
-#             <option value="Miaoli">苗栗</option>
-#             </select>
-#         <br></br>
-#         </form>
-
-#         <form action="/page1">
-
-
-#             <button style="font-size:20px;" type="summit">  無號誌交叉口 </button>
-#         </form>
-
-        
-#         <form action="/page1">
-#             <input type="submit" value="無號誌交叉口" />
-#         </form>
-#         <br></br>
-
-#         <form action="" method='post' style="">
-            
-#             <input type=file name=file>
-#             <br>請輸入搜尋位置<input type=summit value="位置" name="搜尋位置按鈕">
-            
-#         </form>
-#         </div>
-
-#     </body>
-#     </html>
-   
-#     """))
-
-#     map.save("output.html")
-
-#     folium.Marker(
-#         location=[25.030797164609954, 121.55874457225863],
-#         tooltip ="好玩嘗試xddd"
-
-#     ).add_to(map)
-
- 
-#     return map._repr_html_()
-
-# @app.route('/page1')
-# def page():
-#     return render_template('111年臺北市無號誌交叉口原始點資料_縮放人數_new.html')
-
 
 
 if __name__ == '__main__':
